218c0a81c673df069a891aa042150e7c77e25965
David Lai <pf.davidlai@gmail.com>
2023-02-17 17:48:07 -0800

    built out phase 1 and part of phase 2

--- W3D5/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/knight_path_finder.rb
+++ W3D5/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/knight_path_finder.rb
@@ -1,6 +1,8 @@
+require_relative 'tree_node'
+require 'byebug'
 class KnightPathFinder
 
-    attr_reader :position, :root_node
+    attr_reader :position, :root_node, :considered_positions
 
     def self.valid_moves(pos)
         row, col = pos 
@@ -20,16 +22,28 @@ class KnightPathFinder
         @position = position 
         @root_node = PolyTreeNode.new(position) 
         @considered_positions = [position]
+        KnightPathFinder.valid_moves(position)
         #build_move_tree(@root_node)
     end
 
     def new_move_positions(pos)
-        
+        new_moves = KnightPathFinder.valid_moves(pos).select {|pos| !@considered_positions.include?(pos)}
+        @considered_positions.concat(new_moves)
+        new_moves
     end
-
-
     def find_path(position)
-
+    end
+    def build_move_tree
+        # debugger
+        node_q = []
+        moves = new_move_positions(@position)
+        moves.each do |move|
+          new_node = PolyTreeNode.new(move)  
+          new_node.parent = @root_node
+          node_q << new_node
+        end
+        
+        p node_q
     end
     
 
